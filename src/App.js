979d376862d22c5f63f6fe7e5e10080712a6752f
Emanoel Barros <ebarros297@gmail.com>
2022-08-06 18:24:47 -0300

    fill rating based on value props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import './App.css';
-import 'h8k-components';
+import React from "react";
+import "./App.css";
+import "h8k-components";
 import Rating from "./components/Rating";
 
 const title = "Rating Component";
 
 const App = () => {
+  return (
+    <div className="App">
+      <h8k-navbar header={title}></h8k-navbar>
 
-    return (
-        <div className="App">
-            <h8k-navbar header={title}></h8k-navbar>
-
-            <div className="flex align-items-center justify-content-center container">
-                <div className="card pa-16">
-                    <Rating/>
-                </div>
-            </div>
+      <div className="flex align-items-center justify-content-center container">
+        <div className="card pa-16">
+          <Rating value="3" />
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,41 +1,77 @@
-import React from 'react';
-import './Rating.css';
+import React from "react";
+import "./Rating.css";
 
-const Rating = (props) => {
-    const emptyIcon =  '/icons/stars/empty.svg';
-    const filledIcon =  '/icons/stars/filled.svg';
-    const halfFilledIcon =  '/icons/stars/half.svg';
+const defaultEmptyIcon = "/icons/stars/empty.svg";
+const defaultHalfFilledIcon = "/icons/stars/half.svg";
+const defaultFilledIcon = "/icons/stars/filled.svg";
 
-    // Utility function to calculate if the mouse event happened on the left side of the target or the right side.
-    const isLessThanHalf = (event) => {
-        const {target} = event;
-        const boundingClientRect = target.getBoundingClientRect();
-        let mouseAt = event.clientX - boundingClientRect.left;
-        mouseAt = Math.round(Math.abs(mouseAt));
-        return mouseAt <= boundingClientRect.width / 2;
-    };
+const Rating = ({
+  value = undefined,
+  emptyIcon = defaultEmptyIcon,
+  halfFilledIcon = defaultHalfFilledIcon,
+  filledIcon = defaultFilledIcon,
+  steps = 1,
+}) => {
+  // Utility function to calculate if the mouse event happened on the left side of the target or the right side.
+  const isLessThanHalf = (event) => {
+    const { target } = event;
+    const boundingClientRect = target.getBoundingClientRect();
+    let mouseAt = event.clientX - boundingClientRect.left;
+    mouseAt = Math.round(Math.abs(mouseAt));
+    return mouseAt <= boundingClientRect.width / 2;
+  };
 
-    const renderSymbol = () => {
-        return (
-            <img src=""
-                 className="rating-image"
-                 data-testid="rating-icon"
-                 alt="Rate"/>
-        )
+  const renderIcon = (icon) => {
+    return (
+      <img
+        src={icon}
+        className="rating-image"
+        data-testid="rating-icon"
+        alt="Rate"
+      />
+    );
+  };
+
+  const renderSymbol = () => {
+    const symbols = [];
+    const filled = value || 0;
+
+    if (filled === 0) {
+      for (let i = 1; i <= 5; i++) {
+        symbols.push(renderIcon(emptyIcon));
+      }
+    } else {
+      const filledFloor = Math.floor(filled);
+
+      for (let i = 1; i <= filledFloor; i++) {
+        symbols.push(renderIcon(filledIcon));
+      }
+
+      const rest = (filled * 10) % 10;
+
+      if (rest !== 0) {
+        symbols.push(renderIcon(halfFilledIcon));
+      }
+
+      if (filled <= 4) {
+        for (let i = 1; i <= 5 - filledFloor; i++) {
+          symbols.push(renderIcon(emptyIcon));
+        }
+      }
     }
 
-    return (
-        <div
-            tabIndex="0"
-            className="star-rating"
-            data-testid="star-rating-container"
-        >
-            {
-                renderSymbol()
-            }
-        </div>
-    )
-};
+    return symbols;
+  };
 
+  return (
+    <div
+      tabIndex="0"
+      className="star-rating"
+      data-testid="star-rating-container"
+    >
+      {renderSymbol()}
+    </div>
+  );
+};
 
 export default Rating;
